1daa20ac5237a7ddce9b9a8b08c7eb7c24facce5
Dues-ExMachina <anubhabd45@gmail.com>
2025-06-24 01:09:29 +0530

    removed generateStaticParams() from username/page

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -1,31 +1,51 @@
+// import PaymentPage from "@/components/PaymentPage"
+// import { notFound } from "next/navigation";
+// import connectDb from "@/utils/db/connectDb";
+// import User from "@/models/User";
+
+// const Username = async ({ params }) => {
+//     const resolvedParams = await params; // Await params here
+//     await connectDb();
+//     const u = await User.findOne({ username: resolvedParams.username });
+//     if (!u) {
+//         notFound();
+//     }
+//     return (
+//         <>
+//             <PaymentPage username={resolvedParams.username} />
+//         </>
+//     );
+// }
+
+// export default Username;
+
+
+
+// // Dynamic metadata
+// export async function generateStaticParams() {
+//     await connectDb();
+//     const users = await User.find({}, { username: 1 }).lean();
+//     return users.map(user => ({
+//         username: user.username
+//     }));
+// }
+
+export const dynamic = 'force-dynamic'; // ✅ This tells Next.js: "don't pre-render"
+
 import PaymentPage from "@/components/PaymentPage"
 import { notFound } from "next/navigation";
 import connectDb from "@/utils/db/connectDb";
 import User from "@/models/User";
 
 const Username = async ({ params }) => {
-    const resolvedParams = await params; // Await params here
     await connectDb();
-    const u = await User.findOne({ username: resolvedParams.username });
+    const u = await User.findOne({ username: params.username });
+
     if (!u) {
         notFound();
     }
-    return (
-        <>
-            <PaymentPage username={resolvedParams.username} />
-        </>
-    );
+
+    return <PaymentPage username={params.username} />;
 }
 
 export default Username;
-
-
-
-// Dynamic metadata
-export async function generateStaticParams() {
-    await connectDb();
-    const users = await User.find({}, { username: 1 }).lean();
-    return users.map(user => ({
-        username: user.username
-    }));
-}
