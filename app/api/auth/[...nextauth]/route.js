6546435cedb8ca04b162a71da404b574cbb77cb2
Dues-ExMachina <anubhabd45@gmail.com>
2025-06-24 00:53:42 +0530

    Fixed eslint warnings and build issues again added env to vercel

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,56 +1,46 @@
 import NextAuth from 'next-auth'
-// import AppleProvider from 'next-auth/providers/apple'
-// import FacebookProvider from 'next-auth/providers/facebook'
-// import GoogleProvider from 'next-auth/providers/google'
-// import EmailProvider from 'next-auth/providers/email'
 import GithubProvider from 'next-auth/providers/github'
-// import XProvider from 'next-auth/providers/x'
-// import LinkedInProvider from 'next-auth/providers/linkedin'
 import mongoose from 'mongoose'
 import Payment from '@/models/Payment'
 import User from '@/models/User'
 import connectDb from '@/utils/db/connectDb'
-export const authoptions = NextAuth({
+
+export const authOptions = {
     providers: [
-        // OAuth authentication providers...
         GithubProvider({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET
         }),
-
     ],
     callbacks: {
-        async signIn({ user, account, profile, email, credentials }) {
+        async signIn({ user, account, profile }) {
             if (account.provider === 'github') {
-                await connectDb();
-
+                await connectDb()
 
                 const currentUser = await User.findOne({ email: user.email })
 
                 if (!currentUser) {
                     const newUser = new User({
                         email: user.email,
-
                         username: profile.login,
                         profilepic: profile.avatar_url,
                     })
 
                     await newUser.save()
-
                 }
-
             }
             return true
+        },
+        async session({ session }) {
+            await connectDb()
+            const currentUser = await User.findOne({ email: session.user.email })
+            session.user.name = currentUser.name
+            return session
         }
     },
-    async session({ session, user, token }) {
-        await connectDb();
-        // Add user id to the session
-        const currentUser = await User.findOne({ email: session.user.email })
+    secret: process.env.NEXTAUTH_SECRET
+}
 
-        session.user.name = currentUser.name
-        return session
-    }
-})
+const handler = NextAuth(authOptions)
 
-export { authoptions as GET, authoptions as POST }
+export { handler as GET, handler as POST }
